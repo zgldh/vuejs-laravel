b991ba6d118d66ed577783dabef64ed1d8aee346
Zhang Wenbin <zhangwb@shinetechchina.com>
2016-03-22 19:00:14 +0800

    sitenav component

--- frontend/src/components/Main/App.vue
+++ frontend/src/components/Main/App.vue
@@ -1,54 +1,15 @@
 <template>
-  <!-- Following Menu -->
-  <div class="ui large top fixed hidden menu">
-    <div class="ui container">
-      <a class="active item">Home</a>
-      <a class="item">Work</a>
-      <a class="item">Company</a>
-      <a class="item">Careers</a>
-      <div class="right menu">
-        <div class="item">
-          <a class="ui button">Log in</a>
-        </div>
-        <div class="item">
-          <a class="ui primary button">Sign Up</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Sidebar Menu -->
-  <div class="ui vertical inverted sidebar menu">
-    <a class="active item">Home</a>
-    <a class="item">Work</a>
-    <a class="item">Company</a>
-    <a class="item">Careers</a>
-    <a class="item">Login</a>
-    <a class="item">Signup</a>
-  </div>
-  <!-- Page Contents -->
+
+  <site-nav></site-nav>
+
   <div class="pusher">
     <div class="ui inverted vertical masthead center aligned segment">
-      <div class="ui container">
-        <div class="ui large secondary inverted pointing menu">
-          <a class="toc item">
-            <i class="sidebar icon"></i>
-          </a>
-          <a class="active item">Home</a>
-          <a class="item">Work</a>
-          <a class="item">Company</a>
-          <a class="item">Careers</a>
-          <div class="right item">
-            <a class="ui inverted button">Log in</a>
-            <a class="ui inverted button">Sign Up</a>
-          </div>
-        </div>
-      </div>
       <div class="ui text container">
         <h1 class="ui inverted header">
-          Imagine-a-Company
+          有那么一家公司
         </h1>
-        <h2>Do whatever you want when you want to.</h2>
-        <div class="ui huge primary button">Get Started <i class="right arrow icon"></i></div>
+        <h2>任何时候想干什么就干什么</h2>
+        <div class="ui huge primary button">现在开始 <i class="right arrow icon"></i></div>
       </div>
     </div>
     <div class="ui vertical stripe segment">
@@ -100,35 +61,17 @@
 
 <script>
   require('../../semantic/dist/semantic.js')
-  var $ = require('jquery')
-
-  $(document).ready(function () {
-    // fix menu when passed
-    $('.masthead')
-      .visibility({
-        once: false,
-        onBottomPassed: function () {
-          $('.fixed.menu').transition('fade in')
-        },
-        onBottomPassedReverse: function () {
-          $('.fixed.menu').transition('fade out')
-        }
-      })
-    // create sidebar and attach to menu open
-    $('.ui.sidebar').sidebar('attach events', '.toc.item')
-  })
+  var SiteNav = require('./Common/SiteNav')
 
   export default {
-    components: {}
+    components: {
+      SiteNav
+    }
   }
 </script>
 
-<style>
+<style scoped>
   @import "../../semantic/dist/semantic.css";
-
-  .hidden.menu {
-    display: none;
-  }
 
   .masthead.segment {
     min-height: 700px;
@@ -193,23 +136,7 @@
     padding: 5em 0em;
   }
 
-  .secondary.pointing.menu .toc.item {
-    display: none;
-  }
-
   @media only screen and (max-width: 700px) {
-    .ui.fixed.menu {
-      display: none !important;
-    }
-
-    .secondary.pointing.menu .item,
-    .secondary.pointing.menu .menu {
-      display: none;
-    }
-
-    .secondary.pointing.menu .toc.item {
-      display: block;
-    }
 
     .masthead.segment {
       min-height: 350px;
@@ -225,6 +152,4 @@
       font-size: 1.5em;
     }
   }
-
-
 </style>
---
+++ frontend/src/components/Main/Common/SiteNav.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- Sidebar Menu -->
+  <div class="ui vertical sidebar menu">
+    <a class="active item">首页</a>
+    <a class="item">工作</a>
+    <a class="item">公司</a>
+    <a class="item">招聘</a>
+    <a class="item">登录</a>
+    <a class="item">注册</a>
+  </div>
+  <!-- Page Contents -->
+  <div class="pusher site-nav-pusher">
+    <div class="ui large fixed pointing menu site-nav">
+      <div class="ui container">
+        <a class="toc item">
+          <i class="sidebar icon"></i>
+        </a>
+        <a class="active item">首页</a>
+        <a class="item">工作</a>
+        <a class="item">公司</a>
+        <a class="item">招聘</a>
+        <div class="right menu">
+          <a class="item">登录</a>
+          <a class="item">注册</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  var $ = require('jquery')
+
+  $(document).ready(function () {
+    // create sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item')
+  })
+
+  export default {
+    components: {}
+  }
+</script>
+
+<style lang="sass">
+  .pusher.site-nav-pusher {
+    min-height: inherit;
+    overflow: visible;
+    z-index:auto;
+
+    .pointing.menu .toc.item {
+      display: none;
+    }
+
+    @media only screen and (max-width: 700px) {
+      .pointing.menu .item,
+      .pointing.menu .menu {
+        display: none;
+      }
+
+      .pointing.menu .toc.item {
+        display: block;
+      }
+    }
+  }
+
+</style>

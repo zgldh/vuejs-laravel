81e2dbde1ca5f63a9f383f909109f2907fddb951
Zhang Wenbin <zhangwb@shinetechchina.com>
2016-03-30 15:43:43 +0800

    Use history router

--- frontend/src/components/Index/router.js
+++ frontend/src/components/Index/router.js
@@ -4,7 +4,9 @@ import Home from './Pages/Home.vue'
 import Register from './Pages/Register.vue'
 
 Vue.use(VueRouter)
-var router = new VueRouter()
+var router = new VueRouter({
+  history: true
+})
 router.map({
   '/': {
     component: Home

15773e8ab0d7f364fd667a3d7ec31fcae86fb288
Zhang Wenbin <zhangwb@shinetechchina.com>
2016-03-30 15:45:59 +0800

    Remove router history

--- frontend/src/components/Index/router.js
+++ frontend/src/components/Index/router.js
@@ -5,7 +5,10 @@ import Register from './Pages/Register.vue'
 
 Vue.use(VueRouter)
 var router = new VueRouter({
-  history: true
+  // Set to true to remove #! in path.
+  // It needs proper configured server.
+  // http://readystate4.com/2012/05/17/nginx-and-apache-rewrite-to-support-html5-pushstate/
+  history: false
 })
 router.map({
   '/': {
